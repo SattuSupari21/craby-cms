f39feb5092035ad47abf27fa983b28860507bbd9
samtu <41340170+SattuSupari21@users.noreply.github.com>
2024-05-10 13:14:18 +0530

    added shadcn theme provider

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,12 +1,14 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="p-2 flex gap-2">
+            <div className="p-2 flex items-center justify-between gap-2">
                 <Link to="/" className="[&.active]:font-bold">
                     Home
                 </Link>
+                <ModeToggle />
             </div>
             <hr />
             <Outlet />
